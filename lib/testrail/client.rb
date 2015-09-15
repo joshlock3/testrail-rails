042037846a00e5fb5ecfde35b160c4b24e22ab5b
Joshua Lockhart <jlockhart@ncsa-mbp-02.ncsasports.org>
2015-09-15 10:44:52 -0500

    updating testrail client

--- lib/testrail/client.rb
+++ lib/testrail/client.rb
@@ -1,13 +1,40 @@
-require 'testrail/api_client'
-
 module TestRail
   class Client
-    attr_accessor :connection
+    attr_accessor :connection, :url, :username, :password
 
-    def initialize(user,password,url)
-      @connection = TestRail::APIClient.new(url)
-      @connection.user = user
-      @connection.password = password
+    def initialize(username,password,url)
+      @url_endpoint = url || self.class.url
+      @connection = APIClient.new(url)
+      @connection.user = username || self.class.username
+      @connection.password = password || self.class.password
+    end
+
+    def project
+      Project.new(connection)
+    end
+
+    def release
+      Release.new(connection)
+    end
+
+    def section
+      Section.new(connection)
+    end
+
+    def suite
+      Suite.new(connection)
+    end
+
+    def test_case
+      TestCase.new(connection)
+    end
+
+    def test_result
+      TestResult.new(connection)
+    end
+
+    def test_run
+      TestRun.new(connection)
     end
   end
 end
